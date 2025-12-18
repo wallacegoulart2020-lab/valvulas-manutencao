2843d560ba3a016ef24bf366a44bd42f93be23ec
wallacegoulart2020-lab <wallacegoulart2020@gmail.com>
2025-12-17 23:07:26 -0300

    Criar App.jsx na pasta src

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+export default function App() {
+  return (
+    <div style={{ padding: "20px", fontFamily: "Arial" }}>
+      <h1>Painel de Monitoramento de Válvulas</h1>
+
+      <p>
+        Sistema gerencial para registro, histórico e análise de falhas
+        em válvulas industriais.
+      </p>
+
+      <hr />
+
+      <h2>Status Geral</h2>
+      <ul>
+        <li>Linhas monitoradas: 3</li>
+        <li>Válvulas cadastradas: 6</li>
+        <li>Falhas registradas: 0</li>
+      </ul>
+    </div>
+  );
+}
