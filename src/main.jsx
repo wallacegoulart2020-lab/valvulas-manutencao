8be6d5fdc8a9daf1b4717136c905b1349843d038
wallacegoulart2020-lab <wallacegoulart2020@gmail.com>
2025-12-18 19:06:49 -0300

    fix: fundo escuro global (html/body)

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+document.documentElement.style.background = "#1c232b";
+document.body.style.margin = "0";
+document.body.style.background = "#1c232b";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
