bf23c7111c03f62a6bcb7c344a1ec8bf5586f01e
wallacegoulart2020-lab <wallacegoulart2020@gmail.com>
2025-12-17 21:54:50 -0300

    add App.jsx

---
+++ src/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div style={{ padding: "20px", fontFamily: "Arial" }}>
+      <h1>App de Monitoramento de Válvulas</h1>
+      <p>
+        Sistema de registro e histórico de manutenção de válvulas industriais.
+      </p>
+    </div>
+  );
+}
+
+export default App;
